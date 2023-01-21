506cdbb32aa30ae2bef67720721c0cee88a37bbe
incoder000 <nahidimanli223@gmail.com>
2023-01-21 21:39:52 +0400

    second commit

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from core import views
 
 urlpatterns = [path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path(' ContactUs/', views.ContactUs, name='ContuctUs'),
-    path ('Gallery/', views.Gallery, name= 'Gallery'),
-    path ('blogs/', views.blogs, name= 'blogs'),
+    path('contact', views.contact, name='contact'),
+    path(' ContactUs', views.ContactUs, name='ContuctUs'),
+    path ('Gallery', views.Gallery, name= 'Gallery'),
+    path ('blogs', views.blogs, name= 'blogs'),
 ] 
 
 
--- website/settings.py
+++ website/settings.py
@@ -75,27 +75,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'developia',
-        'USER': 'user',
-        'PASSWORD': '12345',
-        'PORT': 5432,
-        'HOST': 'localhost',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
 
-# Password validationc  c
+# Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -139,6 +127,3 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'data/') # 'data' is my media folder
-MEDIA_URL = '/media/'
