41ac61302988c3fd5416a28faf7161e10dcfbdf6
Abdulov Farid <fgb2mg@mail.ru>
2023-01-21 21:06:45 +0400

    test

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from core import views
 
-urlpatterns = [path('', views.home, name='index'),
+urlpatterns = [path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path(' ContactUs/', views.ContactUs, name='ContuctUs'),
     path ('Gallery/', views.Gallery, name= 'Gallery'),
--- website/urls.py
+++ website/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from core import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf import settings
 from core.urls import urlpatterns as core_urls
 from django.conf.urls import include
 
